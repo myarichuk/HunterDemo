def3f9e2ef6dbef901249ff71724217a9137e744
Michael Yarichuk <michael.yarichuk@gmail.com>
2020-01-01 19:34:53 +0200

    initial commit, stuff works

---
+++ PrintImageSizeUtil/PrintImageSizeUtil.cpp
@@ -0,0 +1,13 @@
+﻿// PrintImageSizeUtil.cpp : Defines the entry point for the application.
+//
+
+#include "PrintImageSizeUtil.h"
+
+using namespace std;
+
+int main()
+{
+	const auto img = cv::imread("e:\\Capture.JPG");
+	cout << "image size: " << img.cols << "x" << img.rows << endl;
+	return 0;
+}
